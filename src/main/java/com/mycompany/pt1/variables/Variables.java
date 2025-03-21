64ca6ff869502c262d7949fba8904078fc0511b8
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-03-21 17:32:13 +0100

    Versió inicial

---
+++ src/main/java/com/mycompany/pt1/variables/Variables.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pt1.variables;
+
+/**
+ *
+ * @author Yamila Cazalla Recio
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
