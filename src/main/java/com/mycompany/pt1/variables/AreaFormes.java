ca93d6452f7b39e6346e69c10fa4aab735cc1a0a
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-04-08 21:28:34 +0200

    Correció AreaFormes

--- src/main/java/com/mycompany/pt1/variables/AreaFormes.java
+++ src/main/java/com/mycompany/pt1/variables/AreaFormes.java
@@ -17,14 +17,18 @@ public class AreaFormes {
         Scanner sc = new Scanner(System.in); //Crear Scanner
         double base; //variable nombre decimal
         double altura; //variable nombre decimal
-        System.out.println("--Calculadora d'àrea d'un triangle--"); //Títol del programa
-        System.out.print(" Introdueix la mida de la base: "); //Pregunta sobre la mida de la base
+        //Títol del programa
+        System.out.println("--Calculadora d'àrea d'un triangle--");
+        //Pregunta sobre la mida de la base
+        System.out.print(" Introdueix la mida de la base: ");
         base= sc.nextDouble(); //Introduïr la mida de la base
-        System.out.print(" Introdueix la mida de l'altura: "); //Pregunta sobre la mida de l'altura
+        //Pregunta sobre la mida de l'altura
+        System.out.print(" Introdueix la mida de l'altura: ");
         altura= sc.nextDouble(); //Introduïr la mida de l'altura
         
         double areatriangulo = base * altura / 2; //Càlcul de l'àrea d'un triangle
-        System.out.println("L'àrea del triangle es: " + areatriangulo); //Text que mostra el resultat
+        //Text que mostra el resultat
+        System.out.println("L'àrea del triangle es: " + areatriangulo);
         
     }
 }
