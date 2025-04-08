dd46752b371818fe67d079a6f34bb7355974b3d9
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-04-08 21:24:17 +0200

    Correció PotesAnimals

--- src/main/java/com/mycompany/pt1/variables/PotesAnimals.java
+++ src/main/java/com/mycompany/pt1/variables/PotesAnimals.java
@@ -18,22 +18,36 @@ public class PotesAnimals {
         int formiga; //variable nombre enter
         int aranya; //variable nombre enter
         int cotxinilla; //variable nombre enter
+        // finals per indicar el nombre de cada animal
+        final int FORMIGA_POTES = 6, ARANYA_POTES = 8, COTXINILLA_POTES = 14;
         System.out.println("--Comptador de potes--"); //Títol del programa
-        System.out.print("Introdueixi cuantes formigues hi han: "); //Pregunta sobre la quantitat de formigues
+        //Pregunta sobre la quantitat de formigues
+        System.out.print("Introdueixi cuantes formigues hi han: ");
         formiga= sc.nextInt(); //Introduïr la quantitat de formigues
-        System.out.print("Introdueixi cuantes aranyes hi han: "); //Pregunta sobre la quantitat de aranyes
+        //Pregunta sobre la quantitat de aranyes
+        System.out.print("Introdueixi cuantes aranyes hi han: ");
         aranya= sc.nextInt();//Introduïr la quantitat de aranyes
-        System.out.print("Introdueixi cuantes cotxinilles hi han: "); //Pregunta sobre la quantitat de cotxinilles
+        //Pregunta sobre la quantitat de cotxinilles
+        System.out.print("Introdueixi cuantes cotxinilles hi han: ");
         cotxinilla= sc.nextInt(); //Introduïr la quantitat de cotxinilles
         
-        int pformiga = formiga * 6; //Càlcul del total de potes de les formigues
-        int paranya = aranya * 8; //Càlcul del total de potes de les aranyes
-        int pcotxinilla = cotxinilla * 14; //Càlcul del total de potes de les cotxinilles
-        System.out.println("Hi ha " + pformiga + " potes de formigues."); //Text que mostra la quantitat de potes de les formigues
-        System.out.println("Hi ha " + paranya + " potes de aranyes."); //Text que mostra la quantitat de potes de les aranyes
-        System.out.println("Hi ha " + pcotxinilla + " potes de cotxinilles."); //Text que mostra la quantitat de potes de les cotxinilles
-        int totalpotes = pformiga + paranya + pcotxinilla; //Càlcul del total de les potes de tots els invertebrats
-        System.out.println("La quantitat de potes en total és la següent: " + totalpotes); //Text que mostra el resultat
+        //Càlcul del total de potes de les formigues
+        int pformiga = formiga * FORMIGA_POTES;
+        //Càlcul del total de potes de les aranyes
+        int paranya = aranya * ARANYA_POTES;
+        //Càlcul del total de potes de les cotxinilles
+        int pcotxinilla = cotxinilla * COTXINILLA_POTES;
+        //Text que mostra la quantitat de potes de les formigues
+        System.out.println("Hi ha " + pformiga + " potes de formigues.");
+        //Text que mostra la quantitat de potes de les aranyes
+        System.out.println("Hi ha " + paranya + " potes de aranyes.");
+        //Text que mostra la quantitat de potes de les cotxinilles
+        System.out.println("Hi ha " + pcotxinilla + " potes de cotxinilles.");
+        //Càlcul del total de les potes de tots els invertebrats
+        int totalpotes = pformiga + paranya + pcotxinilla;
+        //Text que mostra el resultat
+        System.out.println("La quantitat de potes en total és la següent: "
+                + totalpotes);
     }
     
 }
